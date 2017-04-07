7887f09af1b14399e3d556738b2b0c0b5449c2e7
Seon Hong Kim <scarletbreez@naver.com>
2017-04-08 01:20:23 +0900

    jquery add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,13 @@
 //= require_tree .
 
 
+    jQuery(document).ready(function($) {
+        $(".scroll").click(function(event){
+                event.preventDefault();
+                $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+            });
+    });
+
 /*
     $(document).ready(function (){
         $("#Home").click(function() {
